bd5e9d32b3d6f427fd089184b8d2b47cc95578de
HPrzybysz <niewica@gmail.com>
2024-09-23 14:50:27 +0200

    first commit

---
+++ 1_dokumentacjaTestowanie/BusinessLogic/Calculator.cs
@@ -0,0 +1,27 @@
+﻿namespace BusinessLogic
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// method of addition
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Add(int a, int b) 
+        {
+            return a + b;
+        }
+        /// <summary>
+        /// method of substraction
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Sub(int a, int b)
+        {
+            return (a - b);
+        }
+    }
+    
+}
---
+++ 1_dokumentacjaTestowanie/dokumentacjaTestowanie/Program.cs
@@ -0,0 +1,5 @@
+﻿using BusinessLogic;
+Calculator calculator = new Calculator();
+
+Console.WriteLine(calculator.Add(2000,137));
+Console.WriteLine(calculator.Sub(20,5));
